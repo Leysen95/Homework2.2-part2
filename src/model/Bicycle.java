b79d57e52ebab63bbfb4f1c14f60ea63e782b134
Leysen95 <mullayanova95@bk.ru>
2024-07-22 21:03:03 +0500

    Initial commit

---
+++ src/model/Bicycle.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
